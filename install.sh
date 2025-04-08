fb145973745a0cba7bd69e7485bdc36648cfbba7
Do1SKL <48216959+Do1SKL@users.noreply.github.com>
2025-04-08 14:28:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Do1SKL/test/blob/main/shell.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/Do1SKL/test/blob/main/shell.sh)"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Do1SKL/test/blob/main/menu.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/Do1SKL/test/blob/main/menu.sh)"
 menu-aprs
